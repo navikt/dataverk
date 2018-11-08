fd31f557541a2dbf10641c43f4b82f85db7c72c8
V153753 <erik.vattekar@nav.no>
2018-11-08 14:19:43 +0100

    Refactor dataverk_create to accept settingsfile in json format, started with unittests

--- dataverk_setup_scripts/dataverk_create.py
+++ dataverk_setup_scripts/dataverk_create.py
@@ -19,10 +19,14 @@ class CreateDataPackage:
                  namespace: str, settings_repo: str,
                  user: str, password: str):
 
+        self._verify_constructor_arguments(name, github_project, update_schedule, namespace, settings_repo, user, password)
+
         self.jenkins_server = jenkins.Jenkins('http://a34apvl00117.devillo.no:8080', username=user, password=password)
 
+        # TODO: Hent jenkins url fra settings
+
         if not self._is_in_repo_root():
-            raise Exception("dataverk_create må kjøres fra topp-nivået i git repoet")
+            raise Exception("dataverk create må kjøres fra topp-nivået i git repoet")
 
         if self._is_package_name_in_use(name):
             raise NameError("Datapakkenavn må være unikt.")
@@ -30,6 +34,7 @@ class CreateDataPackage:
         self.package_name = name
         self.github_project = github_project
         self.namespace = namespace
+        self.cronjob_schedule = update_schedule
 
         self._create_folder_structure()
 
@@ -37,12 +42,28 @@ class CreateDataPackage:
 
         self.settings_folder_name = settings.download_to(self.package_name)
 
-        if update_schedule is None:
-            self.cronjob_schedule = self._set_cronjob_schedule()
-        else:
-            self.cronjob_schedule = update_schedule
+    def _verify_constructor_arguments(self, name, github_project, update_schedule, namespace, settings_repo, user, password):
+        if not isinstance(name, str):
+            raise TypeError("name parameter must be of type string")
 
-        # TODO: verify cronjob_schedule string
+        if not isinstance(github_project, str):
+            raise TypeError("github_project parameter must be of type string")
+
+        if not isinstance(update_schedule, str):
+            raise TypeError("update_schedule parameter must be of type string")
+
+        if not isinstance(namespace, str):
+            raise TypeError("namespace parameter must be of type string")
+
+        if not isinstance(settings_repo, str):
+            raise TypeError("settings_repo parameter must be of type string")
+
+        if not isinstance(user, str):
+            raise TypeError("user parameter must be of type string")
+
+        if not isinstance(password, str):
+            raise TypeError("password parameter must be of type string")
+
 
     def _is_in_repo_root(self):
         ''' Sjekk på om create_dataverk kjøres fra toppnivå i repo.
@@ -72,49 +93,6 @@ class CreateDataPackage:
 
         return False
 
-    def _set_cronjob_schedule(self):
-        ''' Kontrollerer brukerinput og setter cronjob schedule
-
-        :return: String: cronjob-schedule for .yaml fil
-        '''
-
-        print("------------------------------------------------Oppdateringsschedule------------------------------------------")
-        print("Ukedager - man-søn (0-6)")
-        print("MERK: For flere dager, adskill med komma. F.eks. ønsker du at datapakken skal oppdateres mandag,tirsdag og fredag blir dette: \"0,1,4\"")
-        days = input("Skriv inn hvilke(n) ukedag(er) datapakken skal oppdateres: ")
-
-        day_list = days.split(',')
-
-        for day in day_list:
-            if int(day) not in range(0, 7):
-                raise Exception("'" + day + "' er ikke en gyldig dag. Gyldige dager er 0-6 (man-søn)")
-
-        print("\nTime på dagen(e) - 0-23")
-        print("MERK: Time oppgis i UTC")
-        hour = int(input("Skriv inn hvilken time på dagen(e) du vil at datapakken skal oppdateres (UTC tid): "))
-
-        if hour not in range(0, 23):
-            raise Exception("'" + str(hour) + "' er ikke en gyldig time. Gyldig time er 0-23")
-
-        print("\nMinutt innenfor time - 0-59")
-        minute = int(input("Skriv inn hvilket minutt innenfor time datapakken skal oppdateres: "))
-
-        if minute not in range(0, 59):
-            raise Exception("'" + str(minute) + "' er ikke gyldig minutt innnenfor time. Gyldig minutt er 0-59")
-
-        print("--------------------------------------------------------------------------------------------------------------")
-
-        return str(minute) + " " + str(hour) + " " + "* " + "* " + days
-
-
-    def print_datapackage_config(self):
-        print("\n-------------Ny datapakke------------------------" +
-              "\nDatapakkenavn: " + self.package_name +
-              "\ngithub repo: " + self.github_project +
-              "\ncronjob schedule: " + self.cronjob_schedule +
-              "\nNAIS namespace: " + self.namespace +
-              "\n-------------------------------------------------\n")
-
     def _create_folder_structure(self):
         ''' Lager mappestrukturen for datapakken. Oppretter mappene:
             {repo_root}/{pakkenavn}/scripts
@@ -125,14 +103,20 @@ class CreateDataPackage:
         os.mkdir(os.path.join(self.package_name, 'scripts'))
         os.mkdir(os.path.join(self.package_name, 'data'))
 
-    def copy_setup_file(self):
+        if not os.path.exists(os.path.join(self.package_name, 'scripts')):
+            raise Exception("Unable to create path '" + str(os.path.join(self.package_name, 'scripts')) + "'")
+
+        if not os.path.exists(os.path.join(self.package_name, 'data')):
+            raise Exception("Unable to create path '" + str(os.path.join(self.package_name, 'data')) + "'")
+
+    def _copy_setup_file(self):
         '''
         '''
 
-        copyfile(os.path.join(str(self.settings_folder_name), 'settings.py'),
-                 os.path.join(self.package_name, 'settings.py'))
+        copyfile(os.path.join(str(self.settings_folder_name), 'settings.json'),
+                 os.path.join(self.package_name, 'scripts', 'settings.json'))
 
-    def copy_template_files(self):
+    def _copy_template_files(self):
         ''' Kopierer følgende template filer til datapakken:
             - jenkins_config.xml
             - Jenkinsfile
@@ -153,7 +137,7 @@ class CreateDataPackage:
         copyfile(os.path.join(str(self.settings_folder_name), 'file_templates', 'METADATA.json'), os.path.join(self.package_name, 'METADATA.json'))
         copyfile(os.path.join(str(self.settings_folder_name), 'file_templates', 'etl.ipynb'), os.path.join(self.package_name, 'scripts', 'etl.ipynb'))
 
-    def edit_package_metadata(self):
+    def _edit_package_metadata(self):
         '''  Tilpasser metadata fil til datapakken
 
         '''
@@ -167,7 +151,7 @@ class CreateDataPackage:
         with open(os.path.join(self.package_name, 'METADATA.json'), 'w') as metadatafile:
             json.dump(package_metadata, metadatafile)
 
-    def edit_cronjob_config(self):
+    def _edit_cronjob_config(self):
         ''' Tilpasser cronjob config fil til datapakken
 
         '''
@@ -185,7 +169,7 @@ class CreateDataPackage:
         with open(os.path.join(self.package_name, 'cronjob.yaml'), 'w') as yamlfile:
             yamlfile.write(yaml.dump(cronjob_config, default_flow_style=False))
 
-    def edit_jenkins_file(self):
+    def _edit_jenkins_file(self):
         ''' Tilpasser Jenkinsfile til datapakken
 
         '''
@@ -201,7 +185,7 @@ class CreateDataPackage:
         with open(os.path.join(self.package_name, 'Jenkinsfile'), 'w') as jenkinsfile:
             jenkinsfile.write(jenkins_config)
 
-    def create_jenkins_job(self):
+    def _create_jenkins_job(self):
         ''' Tilpasser jenkins konfigurasjonsfil og setter opp ny jenkins jobb for datapakken
 
         '''
@@ -227,23 +211,75 @@ class CreateDataPackage:
             rmtree(self.package_name)
             raise Exception("Klarte ikke sette opp jenkins jobb.")
 
+    def print_datapackage_config(self):
+        print("\n-------------Ny datapakke------------------------" +
+              "\nDatapakkenavn: " + self.package_name +
+              "\ngithub repo: " + self.github_project +
+              "\ncronjob schedule: " + self.cronjob_schedule +
+              "\nNAIS namespace: " + self.namespace +
+              "\n-------------------------------------------------\n")
+
     def create(self):
         ''' Oppretter ny datapakke med ønsket konfigurasjon
 
         '''
 
-        self.copy_setup_file()
-        self.copy_template_files()
-        self.edit_package_metadata()
-        self.edit_cronjob_config()
-        self.edit_jenkins_file()
+        self._copy_setup_file()
+        self._copy_template_files()
+        self._edit_package_metadata()
+        self._edit_cronjob_config()
+        self._edit_jenkins_file()
 
-        self.create_jenkins_job()
+        self._create_jenkins_job()
 
         print("Datapakken " + self.package_name + " er opprettet")
 
         # TODO: Add more error handling
 
+
+def validate_cronjob_schedule(schedule):
+    ''' Kontrollerer brukerinput for cronjob schedule
+            Format på cronjob schedule string: "* * * * *"
+            "minutt" "time" "dag i måned" "måned i år" "ukedag"
+             (0-59)  (0-23)    (1-31)        (1-12)     (0-6)
+    '''
+
+    schedule_list = schedule.split(' ')
+
+    if not len(schedule_list) is 5:
+        raise ValueError(
+            "Schedule '" + schedule + "' har ikke riktig format. Må ha formatet: \"<minutt> <time> <dag i måned> <måned> <ukedag>\". "
+                                      "F.eks. \"0 12 * * 2,4\" vil gi: Hver tirsdag og torsdag kl 12.00 UTC")
+
+    if not schedule_list[0] is '*':
+        for minute in schedule_list[0].split(','):
+            if not int(minute) in range(0, 60):
+                raise ValueError("I schedule " + schedule + " er ikke '" + minute +
+                                 "' en gyldig verdi for minutt innenfor time. Gyldige verdier er 0-59', eller '*'")
+
+    if not schedule_list[1] is '*':
+        for hour in schedule_list[1].split(','):
+            if not int(hour) in range(0, 24):
+                raise ValueError("I schedule " + schedule + " er ikke '" + hour +
+                                 "' en gyldig verdi for time. Gyldige verdier er 0-23, eller '*'")
+
+    if not schedule_list[2] is '*':
+        for day in schedule_list[2].split(','):
+            if not int(day) in range(1, 32):
+                raise ValueError("I schedule " + schedule + " er ikke '" + day +
+                                 "' en gyldig verdi for dag i måned. Gyldige verdier er 1-31, eller '*'")
+
+    if not schedule_list[3] is '*':
+        for month in schedule_list[3].split(','):
+            if not int(month) in range(1, 13):
+                raise ValueError("I schedule " + schedule + " er ikke '" + month +
+                                 "' en gyldig verdi for måned. Gyldige verdier er 1-12, eller '*'")
+
+    if not schedule_list[4] is '*':
+        for weekday in schedule_list[4].split(','):
+            if not int(weekday) in range(0, 7):
+                raise ValueError("I schedule " + schedule + " er ikke '" + weekday +
+                                 "' en gyldig verdi for ukedag. Gyldige verdier er 0-6 (søn-lør), eller '*'")
 
 def get_github_url():
     if not os.popen('git rev-parse --is-inside-work-tree').read().strip():
@@ -253,6 +289,10 @@ def get_github_url():
 
 
 def run(package_name_in: str=None, update_schedule_in: str=None, nais_namespace_in: str=None, settings_repo_in: str=None):
+    ''' Entrypoint for dataverk create
+
+    '''
+
     github_project = get_github_url()
 
     print("Opprettelse av ny datapakke i " + github_project)
@@ -262,28 +302,37 @@ def run(package_name_in: str=None, update_schedule_in: str=None, nais_namespace_
     else:
         datapackage = package_name_in
 
-    if settings_repo_in is None:
-        settings_repo_url = input("Lim inn url til settings repo: ")
+    if update_schedule_in is None:
+        update_schedule = input("Skriv inn ønsket oppdateringsschedule for datapakken "
+                                "(format: \"<minutt> <time> <dag i måned> <måned> <ukedag>\", "
+                                "f.eks. \"0 12 * * 2,4\" vil gi <Hver tirsdag og torsdag kl 12.00 UTC>): ")
     else:
-        settings_repo_url = settings_repo_in
+        update_schedule = update_schedule_in
+
+    validate_cronjob_schedule(update_schedule)
 
     if nais_namespace_in is None:
         namespace = input("Skriv inn ønsket NAIS namespace: ")
     else:
         namespace = nais_namespace_in
 
+    if settings_repo_in is None:
+        settings_repo_url = input("Lim inn url til settings repo: ")
+    else:
+        settings_repo_url = settings_repo_in
+
     user = input("Brukerident: ")
     password = getpass.getpass("Passord: ")
 
     new_datapackage = CreateDataPackage(name=datapackage, github_project=github_project,
-                                        update_schedule=update_schedule_in, settings_repo=settings_repo_url,
+                                        update_schedule=update_schedule, settings_repo=settings_repo_url,
                                         namespace=namespace, user=user, password=password)
 
     new_datapackage.print_datapackage_config()
 
     res = input("Vil du opprette datapakken med konfigurasjonen over? [j/n] ")
 
-    if res == 'j':
+    if res in {'j', 'ja', 'y', 'yes'}:
         new_datapackage.create()
     else:
         print("Datapakken ble ikke opprettet")
