b1d415203b9ef561e8239640c427908dea43dae2
V153753 <erik.vattekar@nav.no>
2018-12-17 14:58:10 +0100

    Add S3 connector to dataverk s3 api service

--- dataverk/connectors/bucket_connector_factory.py
+++ dataverk/connectors/bucket_connector_factory.py
@@ -1,7 +1,7 @@
 from dataverk.connectors.bucket_storage_base import BucketStorageConnector
 from dataverk.connectors.google_storage import GoogleStorageConnector
 from dataverk.connectors.azure_blob_storage import AzureStorageConnector
-from dataverk.connectors.s3 import AWSS3Connector
+from dataverk.connectors.AWS_S3 import S3Connector
 from collections.abc import Mapping
 from enum import Enum
 
@@ -14,7 +14,7 @@ class BucketType(Enum):
 
 def get_storage_connector(bucket_type: BucketType, bucket_name: str, settings: Mapping, encrypted: bool=True) -> BucketStorageConnector:
     if bucket_type == BucketType.AWS_S3:
-        return AWSS3Connector(bucket_name=bucket_name, settings=settings, encrypted=encrypted)
+        return S3Connector(bucket_name=bucket_name, settings=settings, encrypted=encrypted)
     elif bucket_type == BucketType.GCS:
         return GoogleStorageConnector(bucket_name=bucket_name, settings=settings, encrypted=encrypted)
     elif bucket_type == BucketType.AZURE:
