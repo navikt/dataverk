9e99c141301de9f41c443596f364b5fdae6ebfdb
Paul Bencze <paul@idelab.com>
2018-12-19 10:21:31 +0100

    Update get_df_from_sqlite.py

--- examples/get_df_from_sqlite.py
+++ examples/get_df_from_sqlite.py
@@ -2,7 +2,7 @@ import pandas as pd
 from dataverk.connectors.sqlite import SQLiteConnector
 
 # default is in-memory database
-cnx = SQLiteConnector()
+con = SQLiteConnector()
 
 people = [
     {'id': 1, 'name': 'Per'},
@@ -12,18 +12,11 @@ people = [
 df = pd.DataFrame(people)
   
 def sql_lite_roundtrip(df):
-    cnx.persist_pandas_df('people', df)
-    df = cnx.get_pandas_df("select * from people")
+    con.persist_pandas_df('people', df)
+    df = con.get_pandas_df("select * from people")
     return df
 
 res = sql_lite_roundtrip(df)
 print(res)
 
-def query_sqlite(query):
-    df = cnx.get_pandas_df(query)
-    return df
-
-res = query_sqlite("select * from people where id = 3")
-print(res)
-    
-
+df = con.get_pandas_df("select * from people where id = 3")
