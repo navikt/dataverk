3851ec7d4cf0d0a6f29e2ca1a5ea8dcdc89bf85d
Sondre <sondre.halvorsen@nav.no>
2019-01-04 15:29:37 +0100

    Move arg commands to commands.py as functions
    Refactor dataverk_delete to use question function from cli_utils

--- dataverk_cli/dataverk.py
+++ dataverk_cli/dataverk.py
@@ -5,6 +5,7 @@ from .dataverk_base import Action
 from .dataverk_factory import get_datapackage_object
 from .dataverk_notebook2script import notebook2script
 from .dataverk_publish import publish_datapackage
+from . import commands
 
 
 def main():
@@ -16,77 +17,13 @@ def main():
     sub_arg_parser = arg_parser.add_subparsers(title='commands', dest='command')
     sub_arg_parser.required = True
 
-    # Create env file command
-    parser_create_env_file = sub_arg_parser.add_parser('create-env-file', add_help=False)
-    parser_create_env_file.add_argument('-v', '--version', action='version', version=__version__,
-                                        help="Viser programversjon")
-    parser_create_env_file.add_argument('-h', '--help', action='help', help="Viser denne hjelpemeldingen")
-    parser_create_env_file.add_argument('-d', '--destination', dest="destination", action='store', metavar='<path>',
-                                        default=None, help="Sti til ønsket lagringslokasjon for .env fil. "
-                                                           "Dersom denne ikke spesifiseres vil .env filen "
-                                                           "legges i stien som skriptet kjøres fra.")
-
-    # Init command
-    parser_init = sub_arg_parser.add_parser('init', add_help=False)
-    parser_init.add_argument('-v', '--version', action='version', version=__version__,
-                              help="Viser programversjon")
-    parser_init.add_argument('-h', '--help', action='help', help="Viser denne hjelpemeldingen")
-    parser_init.add_argument('-p', '--prompt-missing-args', dest="prompt_missing_args", action='store_true',
-                              help="Prompter bruker om å skrive inn alle settings parametere som ikke "
-                                   "angis som input til skriptet (default settings fil brukes ikke)")
-    parser_init.add_argument('--package-name', dest="package_name", action='store', metavar='<pakkenavn>',
-                              default=None, help="Ønsket navn på ny datapakke")
-    parser_init.add_argument('--update-schedule', dest="update_schedule", action='store', metavar='<schedule>',
-                              default=None, help="Ønsket oppdateringsschedule for datapakke")
-
-    parser_init.add_argument('--nais-namespace', dest="nais_namespace", action='store', metavar='<namespace>',
-                              default=None, help="Namespace på NAIS plattform")
-    parser_init.add_argument('--elastic-private', dest="elastic_private", action='store', metavar='<endpoint>',
-                              default=None, help="Endepunkt for private elastic index")
-    parser_init.add_argument('--aws-endpoint', dest="aws_endpoint", action='store', metavar='<endpoint>',
-                              default=None, help="Endepunkt for AWS S3")
-    parser_init.add_argument('--jenkins-endpoint', dest="jenkins_endpoint", action='store', metavar='<endpoint>',
-                              default=None, help="Endepunkt for jenkins server")
-    parser_init.add_argument('--vault-secrets-uri', dest="vault_secrets_uri", action='store', metavar='<uri>',
-                              default=None, help="URI for vault secrets")
-    parser_init.add_argument('--vault-auth-path', dest="vault_auth_path", action='store', metavar='<path>',
-                              default=None, help="Vault sti for vks auth path")
-    parser_init.add_argument('--vault-kv-path', dest="vault_kv_path", action='store', metavar='<path>',
-                              default=None, help="Vault sti til kv secrets")
-    parser_init.add_argument('--vault-role', dest="vault_role", action='store', metavar='<role>',
-                              default=None, help="Vault role")
-    parser_init.add_argument('--vault-service-account', dest="vault_service_account", action='store', metavar='<service account>',
-                              default=None, help="Vault service account")
-
-    # Schedule command
-    parser_schedule = sub_arg_parser.add_parser('schedule', add_help=False)
-    parser_schedule.add_argument('-v', '--version', action='version', version=__version__,
-                                 help="Viser programversjon")
-    parser_schedule.add_argument('-h', '--help', action='help', help="Viser denne hjelpemeldingen")
-    parser_schedule.add_argument('--package-name', dest="package_name", action='store', metavar='<pakkenavn>',
-                                 default=None, help="Navn på datapakke som ønskes schedulert")
-    parser_schedule.add_argument('--update-schedule', dest="update_schedule", action='store', metavar='<update schedule>',
-                                 default=None, help="Oppdateringsfrekvens for datapakke")
-
-    # Delete command
-    parse_delete = sub_arg_parser.add_parser('delete', add_help=False)
-    parse_delete.add_argument('-v', '--version', action='version', version=__version__,
-                              help="Viser programversjon")
-    parse_delete.add_argument('-h', '--help', action='help', help="Viser denne hjelpemeldingen")
-    parse_delete.add_argument('--package-name', dest="package_name", action='store', metavar='<pakkenavn>',
-                              default=None, help="Navn på datapakke som ønskes fjernet")
-
-    # Notebook2script command
-    parse_notebook2script = sub_arg_parser.add_parser('notebook2script', add_help=False)
-    parse_notebook2script.add_argument('-v', '--version', action='version', version=__version__,
-                              help="Viser programversjon")
-    parse_notebook2script.add_argument('-h', '--help', action='help', help="Viser denne hjelpemeldingen")
-
-    # Publish command
-    parse_publish = sub_arg_parser.add_parser('publish', add_help=False)
-    parse_publish.add_argument('-v', '--version', action='version', version=__version__,
-                               help="Viser programversjon")
-    parse_publish.add_argument('-h', '--help', action='help', help="Viser denne hjelpemeldingen")
+    # command parsers
+    commands.add_create_env_file_command(sub_arg_parser)
+    commands.add_delete_command(sub_arg_parser)
+    commands.add_init_command(sub_arg_parser)
+    commands.add_notebook2script_command(sub_arg_parser)
+    commands.add_publish_command(sub_arg_parser)
+    commands.add_update_schedule_command(sub_arg_parser)
 
     args = arg_parser.parse_args()
 
---
+++ tests/dataverk_cli/test_commands.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+# Import statements
+# =================
+import unittest
+import argparse
+from dataverk_cli import commands
+
+
+# Common input parameters
+# =======================
+
+# Base classes
+# ============
+class Base(unittest.TestCase):
+    """
+    Base class for tests
+
+    This class defines a common `setUp` method that defines attributes which are used in the various tests.
+    """
+    def setUp(self):
+        self.parser = argparse.parser = argparse.ArgumentParser()
+        self.sub_parser = self.parser.add_subparsers(title='commands', dest='command')
+
+
+# Test classes
+# ============
+class Instantiation(Base):
+    """
+    Tests all aspects of instantiation
+
+    Tests include: instantiation with args of wrong type, instantiation with input values outside constraints, etc.
+    """
+    pass
+
+    # Input arguments wrong type
+    # ==========================
+
+    # Input arguments outside constraints
+    # ===================================
+
+
+class Set(Base):
+    """
+    Tests all aspects of setting attributes
+
+    Tests include: setting attributes of wrong type, setting attributes outside their constraints, etc.
+    """
+    pass
+
+    # Set attribute wrong type
+    # ========================
+
+    # Set attribute outside constraint
+    # ================================
+
+
+class MethodsInput(Base):
+    """
+    Tests methods which take input parameters
+
+    Tests include: passing invalid input, etc.
+    """
+    pass
+
+
+class MethodsReturnType(Base):
+    """
+    Tests methods' output types
+    """
+    pass
+
+
+class MethodsReturnValues(Base):
+    """
+    Tests values of methods against known values
+    """
+
+    def test_schedule__normal_case(self):
+        commands.add_update_schedule_command(self.sub_parser)
+        res = self.parser.parse_args(("schedule", "--package-name", "test"))
+        self.assertEqual(res.package_name, "test")
+
