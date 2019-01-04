fbe6c353ed338f11dbd0b03147bd6162d486350e
= <=>
2019-01-04 10:42:57 +0100

    Add from scheduler reader
    Reads scheduler data from settings_store

--- dataverk_cli/scheduling/scheduler_factory.py
+++ dataverk_cli/scheduling/scheduler_factory.py
@@ -9,10 +9,27 @@ class Schedulers(Enum):
     CIRCLE_CI = 3
 
 
-def create_scheduler(scheduler: Schedulers, settings_store: Mapping, env_store: Mapping=None):
+def create_scheduler(settings_store: Mapping, env_store: Mapping=None):
+
     if env_store is None:
         env_store = {}
-    if scheduler is Schedulers.JENKINS:
+    if settings_store is None:
+        raise TypeError("settings_store cannot be None")
+
+    scheduler_type = read_scheduler_from_settings(settings_store)
+
+    # Factory
+    if scheduler_type is Schedulers.JENKINS:
         return JenkinsJobScheduler(settings_store, env_store)
     else:
-        raise KeyError(f"Scheduler({scheduler}) could not be found")
+        raise KeyError(f"Scheduler matching Schedulers({scheduler_type}) could not be found")
+
+
+def read_scheduler_from_settings(settings_store: Mapping):
+    if settings_store.get("jenkins") is not None:
+        return Schedulers.JENKINS
+    if settings_store.get("travis") is not None:
+        return Schedulers.TRAVIS
+    if settings_store.get("circle_ci") is not None:
+        return Schedulers.CIRCLE_CI
+    raise LookupError(f"settings_store({settings_store} does not contain valid scheduler configurations)")
