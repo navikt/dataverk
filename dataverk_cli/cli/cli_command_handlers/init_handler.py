c9d95154c9e6be23af820b84b6ab6e359af8fbd6
V153753 <erik.vattekar@nav.no>
2019-01-28 11:07:32 +0100

    add common config getter function for cli handlers

--- dataverk_cli/cli/cli_command_handlers/init_handler.py
+++ dataverk_cli/cli/cli_command_handlers/init_handler.py
@@ -8,7 +8,7 @@ from dataverk_cli.cli.cli_utils import user_input
 from collections.abc import MutableMapping, Mapping
 
 
-def handle(args, settings_store: MutableMapping, env_store: Mapping) -> (MutableMapping, Mapping):
+def handle(args, settings_store: MutableMapping) -> MutableMapping:
     """
     Handles the init command case. Configures the settings mapping object accordingly to requirements for initialization
     of a new datapackage project
@@ -26,9 +26,7 @@ def handle(args, settings_store: MutableMapping, env_store: Mapping) -> (Mutable
 
     validate_datapackage_name(settings_store["package_name"])
 
-    if user_input.cli_question(f'DO you want to create the datapackage ({settings_store["package_name"]})? [y/n] '):
-        return settings_store, env_store
+    if user_input.cli_question(f'Do you want to create the datapackage ({settings_store["package_name"]})? [y/n] '):
+        return settings_store
     else:
         raise KeyboardInterrupt(f'Datapakken {settings_store["package_name"]} was not created')
-
-
--- dataverk_cli/cli/cli_command_handlers/schedule_handler.py
+++ dataverk_cli/cli/cli_command_handlers/schedule_handler.py
@@ -1,17 +1,13 @@
 """ Handles the schedule command"""
 
-import argparse
 import subprocess
-from dataverk_cli.cli.cli_utils import env_store_functions
 from dataverk_cli.cli.cli_utils import setting_store_functions
 from dataverk.utils.validators import validate_cronjob_schedule
 from dataverk_cli.cli.cli_utils import user_input
-from dataverk_cli.dataverk_factory import get_datapackage_object
-from dataverk_cli.dataverk_base import Action
-from collections.abc import Mapping
+from collections.abc import Mapping, MutableMapping
 
 
-def handle(args) -> (Mapping, Mapping):
+def handle(args, settings_dict: MutableMapping) -> MutableMapping:
     """
     Handles the schedule command case. Configures the settings mapping object accordingly to requirements for
     scheduling of a datapackage project in a datapipeline
@@ -21,29 +17,18 @@ def handle(args) -> (Mapping, Mapping):
     """
 
     if not datapackage_exists_in_remote_repo():
-        raise FileNotFoundError(f'Datapakken må eksistere i remote repositoriet før man kan eksekvere '
-                                f'<dataverk-cli schedule>. git add->commit->push av datapakken og prøv på nytt.')
-
-    settings_dict = setting_store_functions.get_settings_dict()
-    env_store = get_env_store(settings=settings_dict)
+        raise FileNotFoundError(f'The datapackage must exist in a remote repository before <dataverk-cli schedule> '
+                                f'can be executed. Create a remote repository and run git add->commit->push and retry.')
 
     update_schedule = _create_update_schedule(args)
 
     validate_cronjob_schedule(update_schedule)
     settings_dict["update_schedule"] = update_schedule
 
-    if user_input.cli_question('Vil du sette opp pipeline for datapakken? [j/n] '):
-        return settings_dict, env_store
+    if user_input.cli_question('Do you want to set up pipeline for the datapackage? [y/n] '):
+        return settings_dict
     else:
-        KeyboardInterrupt(f'Pipeline for datapakke {settings_dict["package_name"]} ble ikke opprettet')
-
-
-def get_env_store(settings: Mapping):
-    # Is project internal
-    if "internal" in settings:
-        return env_store_functions.create_env_store()
-    else:
-        return {}
+        KeyboardInterrupt(f'Pipeline for datapackage {settings_dict["package_name"]} was NOT created')
 
 
 def datapackage_exists_in_remote_repo():
@@ -56,9 +41,9 @@ def datapackage_exists_in_remote_repo():
 
 def _create_update_schedule(args):
     if args.update_schedule is None:
-        update_schedule = input("Skriv inn ønsket oppdateringsschedule for datapakken "
-                                "(format: \"<minutt> <time> <dag i måned> <måned> <ukedag>\", "
-                                "f.eks. \"0 12 * * 2,4\" vil gi <Hver tirsdag og torsdag kl 12.00 UTC>): ")
+        update_schedule = input("Enter desired update schedule for datapackage "
+                                "(format: \"<minute> <hour> <day of month> <month> <weekday>\", "
+                                "e.g. \"0 12 * * 2,4\" gives <Every Tuesday and Thursday at 12.00 UTC>): ")
         if not update_schedule:
             update_schedule = "* * 31 2 *"  # Default value Feb 31 (i.e. never)
     else:
