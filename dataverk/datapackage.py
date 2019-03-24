50b93332c17926cfaf00bde11a06a46a58155941
pbencze <paul@idelab.com>
2019-03-24 23:46:14 +0100

    start refactoring settings and storage

--- dataverk/datapackage.py
+++ dataverk/datapackage.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import json
+import requests
 import datetime
-from dataverk.connectors import OracleConnector, SQLiteConnector
+from enum import Enum
+from dataverk.connectors import OracleConnector, SQLiteConnector, StorageConnector
 from dataverk.utils import resource_discoverer, notebook2script, get_notebook_name
 from dataverk.context import settings
 from dataverk.utils.validators import validate_bucket_name, validate_datapackage_name
@@ -9,17 +11,34 @@ from pathlib import Path
 from dataverk.context import EnvStore
 from collections.abc import MutableMapping, MutableSequence
 
+class Context(Enum):
+    LOCAL = "local"
+    GITHUB = "github"
+    #GCS = "gcs"
+    #NAIS = "nais"
+    #AWS = "aws"
+    #AZURE = "azure"
 
 class Datapackage:
 
-    def __init__(self, resource_files: dict=None, search_start_path: str="."):
+    def __init__(self, resource_files: dict=None, search_start_path: str=".", settings=None):
         self.is_public = False
         self.resources = {}
         self.views = []
+        self.search_start_path = search_start_path
         self.dir_path = self._package_top_dir()
+
+        try:
+            self.context = settings['context']
+        except:
+            self.context = Context.LOCAL
+            pass
+
         self.datapackage_metadata = self._create_datapackage()
 
-        if resource_files is not None:
+        # TODO: look for settings in 1) parameter 2) path/bucket/vault uri in env variable 'DATAVERK_SETTINGS' 3) file in same directory 
+
+        """  if resource_files is not None:
             self.resource_files = resource_files
         else:
             self.resource_files = resource_discoverer.search_for_files(start_path=Path(search_start_path),
@@ -32,10 +51,134 @@ class Datapackage:
 
         try: 
             self.settings = settings.singleton_settings_store_factory(settings_file_path=Path(self.resource_files["settings.json"]),
-                                                                      env_store=env_store)
+                                                                      env_store=env_store) 
         except:
             self.settings = None
             pass
+        """
+
+        self.settings = settings
+    
+    def _package_top_dir(self) -> Path:
+        return Path(self.search_start_path).resolve() #.parent.absolute()
+    
+    def _get_metadata(self): 
+
+        if self.context == Context.GITHUB:
+            return self._get_metadata_from_repo()
+        else:
+            return self._get_metadata_from_local_files()
+
+    def _get_local_file(self, file):
+
+        try:
+            with self.dir_path.joinpath(file).open(mode='r', encoding="utf-8") as f:
+                return f.read()
+        except OSError:
+            return None
+
+
+    def _get_metadata_from_local_files(self):
+
+        metadata = self._get_local_file('METADATA.json')
+        licence =  self._get_local_file('LICENSE.md')
+        readme =  self._get_local_file('README.md')
+        requirements = self._get_local_file('requirements.txt')
+
+        if metadata is None:
+            metadata = {}
+        elif type(metadata) is object:
+            return metadata
+        else:
+            try:
+                metadata = json.loads(metadata)
+            except:
+                metadata = {}
+
+        if licence is not None:
+            metadata['license'] = license
+
+        if readme is not None:
+            metadata['readme'] = readme
+        
+        if requirements is not None:
+            metadata['requirements'] = requirements
+
+        return metadata
+        
+
+    def _get_from_repo(self, repo, file):
+
+        try:
+            r = requests.get(f'{repo}/{file}')
+            if r.ok:
+                return r.text()
+        except OSError:
+            return None #f'{file} not in repo {repo}'
+ 
+
+    def _get_metadata_from_repo(self):
+
+        repo = self.settings['repo']
+
+        if repo is None:
+            return {}
+
+        licence =  self._get_from_repo(repo, 'LICENSE.md')
+        readme =  self._get_from_repo(repo, 'README.md')
+        metadata =  self._get_from_repo(repo, 'METADATA.json')
+        requirements =  self._get_from_repo(repo, 'requirements.txt')
+
+        if metadata is None:
+            metadata = {}
+        elif type(metadata) is object:
+            return metadata
+        else:
+            try:
+                metadata = json.loads(metadata)
+            except:
+                metadata = {}
+
+        if licence is not None:
+            metadata['license'] = license
+
+        if readme is not None:
+            metadata['readme'] = readme
+        
+        if requirements is not None:
+            metadata['requirements'] = requirements
+
+        return metadata
+
+    
+    def _create_datapackage(self):
+
+        metadata = self._get_metadata()
+
+        if metadata.get('public', False) is True:
+            self.is_public = True
+     
+        metadata['updated'] = datetime.datetime.utcnow()
+        metadata['version'] = "0.0.1"
+   
+        metadata['bucket_name'] = metadata.get('bucket_name', 'default-bucket-nav-opendata')
+
+ 
+        validate_bucket_name(metadata["bucket_name"])
+        # TODO: Kan vi tillate '/' ?
+        # validate_datapackage_name(metadata["datapackage_name"])
+
+        try:
+            with self.dir_path.joinpath('METADATA.json').open(mode='w', encoding="utf-8") as f:
+                f.write(json.dumps(metadata, indent=2))
+        except:
+            pass
+                
+        # TODO: drop?
+        datapackage_metadata = {'readme': metadata['readme']}
+        datapackage_metadata.update(metadata)
+
+        return datapackage_metadata
 
     def _verify_add_resource_input_types(self, df, dataset_name, dataset_description):
         if not isinstance(df, pd.DataFrame):
@@ -75,21 +218,23 @@ class Datapackage:
         :param row_limit:
         :return: None
         """
+        
         if spec is None:
             spec = {"type": type,
                     "group": group,
                     "series": series}
 
         view = {'name': name,
-                'title': title,
-                'description': description,
-                'attribution': attribution,
-                'resources': resources,
-                'specType': spec_type,
-                'spec': spec,
-                'transform': {
-                    "limit": row_limit
-                }}
+                    'title': title,
+                    'description': description,
+                    'attribution': attribution,
+                    'resources': resources,
+                    'specType': spec_type,
+                    'spec': spec,
+                    'transform': {
+                        "limit": row_limit
+                    }
+                }
 
         self.views.append(view)
 
@@ -110,8 +255,6 @@ class Datapackage:
         self._verify_update_metadata_input_types(key, value)
         self.datapackage_metadata[key] = value
 
-    def _package_top_dir(self) -> Path:
-        return Path(".").parent.absolute()
 
     def _is_sql_file(self, source):
         if '.sql' in source:
@@ -124,7 +267,7 @@ class Datapackage:
         """
 
         if connector == 'Oracle':
-            conn = OracleConnector(source=source, settings=self.settings)
+            conn = OracleConnector(source=source, settings_store=self.settings)
 
             if self._is_sql_file(source):
                 df = conn.get_pandas_df(source)
@@ -156,11 +299,12 @@ class Datapackage:
                               dataset_name=dataset_name,
                               dataset_description=dataset_description)
 
+
     def to_sql(self, df, table, schema, sink, connector='Oracle'):
         """Write records in dataframe to a SQL database table"""
 
         if connector == 'Oracle':
-            conn = OracleConnector(source=sink, settings=self.settings)
+            conn = OracleConnector(source=sink, settings_store=self.settings)
             return conn.persist_pandas_df(table, schema, df)
 
         elif connector == 'SQLlite':
@@ -187,72 +331,45 @@ class Datapackage:
             'schema': {'fields': fields}
         }
 
-    def _create_datapackage(self):
-        today = datetime.date.today().strftime('%Y-%m-%d')
 
-        try:
-            with self.dir_path.joinpath('LICENSE.md').open(mode='r', encoding="utf-8") as f:
-                license = f.read()
-        except OSError:
-            license = "No LICENSE file available"
-
-        try:   
-            with self.dir_path.joinpath('README.md').open(mode='r', encoding="utf-8") as f:
-                readme = f.read()
-        except OSError:
-            readme = "No README file available"
-
-        try:
-            with self.dir_path.joinpath('METADATA.json').open(mode='r', encoding="utf-8") as f:
-                metadata = json.loads(f.read())
-        except OSError:
-            metadata = {}
-
-        if metadata.get('public', False) is True:
-            self.is_public = True
-     
-        metadata['updated'] = today
-        metadata['version'] = "0.0.1"
-        metadata['license'] = license
-        metadata['bucket_name'] = metadata.get('bucket_name', 'default-bucket-nav-opendata')
-
-        validate_bucket_name(metadata["bucket_name"])
-        validate_datapackage_name(metadata["datapackage_name"])
-
-        with self.dir_path.joinpath('METADATA.json').open(mode='w', encoding="utf-8") as f:
-            f.write(json.dumps(metadata, indent=2))
-
-        datapackage_metadata = {'readme': readme}
-        datapackage_metadata.update(metadata)
-
-        return datapackage_metadata
-
-    def write_datapackage(self):
+    def write_datapackage(self, storage=None):
         """
-        Writes the Datapackage object to output files, the datapackage files can then be published.
+        Persists the datapackage and related resources to local file system or to storage.
 
         :return: None
         """
         resources = []
-        with self.dir_path.joinpath('datapackage.json').open('w') as outfile:
-            for filename, df in self.resources.items():
-                # TODO bruk Parquet i stedet for csv?
-                resources.append(self._get_csv_schema(df, filename))
+        for filename, df in self.resources.items():
+            # TODO bruk Parquet i stedet for csv?
+            resources.append(self._get_csv_schema(df, filename))
 
-            self.datapackage_metadata['resources'] = resources
+        self.datapackage_metadata['resources'] = resources
+        self.datapackage_metadata['views'] = self.views
 
-            self.datapackage_metadata['views'] = self.views
-
-            json.dump(self.datapackage_metadata, outfile, indent=2, sort_keys=True)
+        if storage is None: # write to local file
+            with self.dir_path.joinpath('datapackage.json').open('w') as outfile:
+                json.dump(self.datapackage_metadata, outfile, indent=2, sort_keys=True)
 
             data_path = self.dir_path.joinpath('resources/')
             if not data_path.exists():
                 data_path.mkdir()
 
             for filename, df in self.resources.items():
-                df.to_csv(data_path.joinpath(filename + '.csv'), index=False, sep=',')
+                df.to_csv(data_path.joinpath(f'{filename}.csv'), index=False, sep=',')
+        else: # write to storage
+            try:
+                connector = StorageConnector(storage=storage, settings=self.settings)
+                connector.write(self.datapackage_metadata, f'{self.repo}/datapackage.json')
+                for filename, df in self.resources.items():
+                    stream = io.StringIO()
+                    df.to_csv(stream, index=False, sep=',')
+                    connector.write(stream, f'{self.repo}/{filename}.csv')
+            except:
+                pass
 
-        try: #convert etl.ipynb notebook to etl.py when run in notebook
+
+    def _convert_notebook_to_code(self): 
+        try: #convert etl.ipynb notebook to etl.py when running in notebook
             shell = get_ipython().__class__.__name__
             if shell == 'ZMQInteractiveShell':
                 notebook2script() 
