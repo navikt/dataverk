4882ae926df52fca5014f1c99d5a8a20b060ecce
Sondre <sondre.halvorsen@nav.no>
2019-04-24 16:29:03 +0200

    Remove passing of encrypted parameter to BucketStorageBase superclass

--- src/dataverk/connectors/abc/bucket_storage_base.py
+++ src/dataverk/connectors/abc/bucket_storage_base.py
@@ -5,7 +5,7 @@ from dataverk.connectors.abc.base import BaseConnector
 class BucketStorageConnector(BaseConnector, ABC):
 
     def __init__(self, encrypted=True):
-        super().__init__(encrypted=encrypted)
+        super().__init__()
 
     def write(self, source_string: str, destination_blob_name: str, fmt: str, metadata: dict={}):
         raise NotImplementedError(f'Abstract method. Needs to be implemented in subclass')
