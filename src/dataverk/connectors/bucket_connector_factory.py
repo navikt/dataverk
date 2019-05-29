feb16fee28e3afd7e3d10cc91aa9daaf53ac7f77
V153753 <erik.vattekar@nav.no>
2019-05-29 13:04:41 +0200

    get s3_endppoint url from environment variable in bucket connector factory

--- src/dataverk/connectors/bucket_connector_factory.py
+++ src/dataverk/connectors/bucket_connector_factory.py
@@ -4,6 +4,7 @@ from dataverk.connectors.azure_blob_storage import AzureStorageConnector
 from collections.abc import Mapping
 from enum import Enum
 from dataverk.connectors.s3 import S3Connector
+from os import environ
 
 
 class BucketType(str, Enum):
@@ -14,7 +15,11 @@ class BucketType(str, Enum):
 
 def get_storage_connector(bucket_type: BucketType, bucket_name: str, settings: Mapping, encrypted: bool=True) -> BucketStorageConnector:
     if bucket_type == BucketType.NAIS:
-        return S3Connector(bucket_name=bucket_name, s3_endpoint=settings["bucket_storage_connections"]["dataverk_s3"]["host"])
+        try:
+            environ["DATAVERK_BUCKET_ENDPOINT"]
+        except KeyError:
+            raise EnvironmentError("DATAVERK_BUCKET_ENDPOINT environment variable must be set")
+        return S3Connector(bucket_name=bucket_name, s3_endpoint=environ["DATAVERK_BUCKET_ENDPOINT"])
     elif bucket_type == BucketType.GCS:
         return GoogleStorageConnector(bucket_name=bucket_name, settings=settings)
     elif bucket_type == BucketType.LOCAL:
