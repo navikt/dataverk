0a588bce44db29cd926169474b90c8fe5afb4d85
Vattekar <42180319+erikvatt@users.noreply.github.com>
2019-06-20 10:53:22 +0200

    add unittests

--- tests/dataverk/connectors/test_kafka_connector.py
+++ tests/dataverk/connectors/test_kafka_connector.py
@@ -2,9 +2,13 @@
 # Import statements
 # =================
 import math
+import avro
 from datetime import datetime
 from unittest import TestCase
 from dataverk.connectors import KafkaConnector
+
+MOCK_SCHEMA = "{\"type\":\"record\",\"name\":\"testRecord\",\"namespace\":\"no.navikt\",\"doc\":\"Schematest\",\"fields\":[{\"name\":\"RandomNumber\",\"type\":\"int\",\"doc\":\"doc\"},{\"name\":\"RandomNumber2\",\"type\":\"long\",\"doc\":\"doc\"}]}"
+MOCK_INVALID_SHCEMA = ""
 
 
 class MockKafkaConsumer:
@@ -17,10 +21,15 @@ class MockMessage:
 
     def __init__(self):
         self._timestamp = int(datetime.now().timestamp() * 1000)
+        self._value = b'\x00\x00\x00\x00\x05\xf4;\x80\xc6\xeb\xb3\xd5P'
 
     @property
     def timestamp(self):
         return self._timestamp
+
+    @property
+    def value(self):
+        return self._value
 
 
 class Base(TestCase):
@@ -55,3 +64,31 @@ class MethodsReturnValues(Base):
         self.conn._read_until_timestamp = self.conn._get_current_timestamp_in_ms() + 1
         res = self.conn._is_requested_messages_read(message=message, max_mesgs=math.inf, mesgs_read=1)
         self.assertFalse(res)
+
+    def test__decode_avro_message(self):
+        message = MockMessage()
+        mesg = self.conn._decode_avro_message(MOCK_SCHEMA, message)
+
+        self.assertTrue("RandomNumber" in mesg.keys())
+        self.assertTrue("RandomNumber2" in mesg.keys())
+        self.assertEqual(mesg["RandomNumber"], 3834)
+        self.assertEqual(mesg["RandomNumber2"], 1385852400000)
+
+    def test__decode_avro_message_invalid_schema(self):
+        message = MockMessage()
+        with self.assertRaises(avro.schema.SchemaParseException):
+            mesg = self.conn._decode_avro_message(MOCK_INVALID_SHCEMA, message)
+
+    def test__extract_requested_fields(self):
+        mesg = MockMessage()
+        decoded_mesg = self.conn._decode_avro_message(MOCK_SCHEMA, mesg)
+        fields = ["RandomNumber", "RandomNumber2"]
+        res = self.conn._extract_requested_fields(mesg=decoded_mesg, fields=fields)
+        self.assertTrue(None not in res)
+
+    def test__extract_requested_fields_not_found(self):
+        mesg = MockMessage()
+        decoded_mesg = self.conn._decode_avro_message(MOCK_SCHEMA, mesg)
+        fields = ["NoneExistantField"]
+        res = self.conn._extract_requested_fields(decoded_mesg, fields)
+        self.assertTrue(None in res)
