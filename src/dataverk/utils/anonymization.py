fc0a6691c4a527b55ebc3e84aaa475a1d4e0e0e4
Cabrol <maria.cabrol@nav.no>
2019-10-25 12:39:06 +0200

    replace values to anonymize by chosen value(s) with tests

--- src/dataverk/utils/anonymization.py
+++ src/dataverk/utils/anonymization.py
@@ -4,26 +4,31 @@ import pandas as pd
 import requests
 
 
-def anonymize_replace(df, eval_column, additional_columns=None, lower_limit=4) -> pd.DataFrame:
-    """ Replace values in columns with "*" when the value is less than lower_limit
+def anonymize_replace(df, eval_column, additional_columns=None, lower_limit=4, replace_by="*") -> pd.DataFrame:
+    """ Replace values in columns when value in eval_column is less than lower_limit
 
     :param df: pandas DataFrame
-    :param eval_column: column to evaluate for anonymization
-    :param additional_columns: list of columns to anonymize if value in eval_column is below lower_limit
-    :param lower_limit: lower limit for value replacement in data set column
+    :param eval_column: str, column to evaluate for anonymization
+    :param additional_columns: str or list of columns to anonymize if value in eval_column is below lower_limit, default=None
+    :param lower_limit: int, lower limit for value replacement in data set column, default=4
+    :param replace_by: str, list or dict, value to replace by. List or dict passed must have same length as number of
+                       columns to anonymize. Values in list must also be given in same order as [additional_columns]+[eval_column]
+
+
     :return: anonymized pandas DataFrame
     """
-    return _replace(df, eval_column, additional_columns, lower_limit)
+    return _replace(df, eval_column, additional_columns, lower_limit, replace_by)
 
 
-def _replace(df: pd.DataFrame, eval_column: str, additional_columns, lower_limit):
+def _replace(df: pd.DataFrame, eval_column: str, additional_columns, lower_limit, replace_by):
 
     columns = _set_columns_to_anonymize(df, eval_column, additional_columns)
 
     _check_value_types(df, eval_column, lower_limit)
+    columns, replace_by = _check_replace_by(columns, replace_by)
 
     to_anonymize = df.copy()
-    return _replace_value(to_anonymize, eval_column, columns, lower_limit)
+    return _replace_value(to_anonymize, eval_column, columns, lower_limit, replace_by)
 
 
 def _set_columns_to_anonymize(df, eval_column, additional_columns):
@@ -51,8 +56,10 @@ def _check_additional_columns_type(additional_columns):
 
 def _check_column_names(df, columns):
     for column in columns:
+        if not isinstance(column, str):
+            raise TypeError(f"{column}: column names should be of type str")
         if column not in df.columns:
-            raise ValueError(f"\'{column}\' is not a column in df")
+            raise ValueError(f"'{column}' is not a column in df")
 
 
 def _check_value_types(df, eval_column, lower_limit):
@@ -63,8 +70,43 @@ def _check_value_types(df, eval_column, lower_limit):
         raise TypeError("lower_limit should be of type int or float")
 
 
-def _replace_value(df, eval_column, columns, lower_limit):
-    df.loc[df[df[eval_column] < lower_limit].index, columns] = "*"
+def _check_replace_by(columns, replace_by):
+    column_order, replace_by_order = columns, replace_by
+
+    if not isinstance(replace_by, (int, float, type(None), str, list, dict)):
+        raise TypeError("values to replace by should be given as types int, float, None or str or in list or dict")
+
+    if isinstance(replace_by, list):
+        if len(replace_by) != len(columns):
+            raise Exception("number of replacement values in list of values to replace_by is different from number of columns to anonymize")
+        for replace_value in replace_by:
+            if not isinstance(replace_value, (int, float, type(None), str)):
+                raise TypeError("values in list of values to replace by should be given as types int, float, None or str")
+
+    if isinstance(replace_by, dict):
+        replace_in_columns = list(replace_by.keys())
+        replace_by_values = list(replace_by.values())
+
+        if len(replace_in_columns) != len(columns):
+            raise Exception("replace_by dictionary should have the same number of keys as columns to anonymize")
+
+        for r_column in replace_in_columns:
+            if not isinstance(r_column, str):
+                raise TypeError(f"{r_column}: keys in replace_by dict should be of type str")
+            if r_column not in columns:
+                raise ValueError(f"{r_column} in replace_by dictionary not in columns to anonymize")
+
+        for replacement_value in replace_by_values:
+            if not isinstance(replacement_value, (int, float, type(None), str)):
+                raise TypeError("values in replace_by dictionary should be of types int, float, None or str")
+
+        column_order, replace_by_order = replace_in_columns, replace_by_values
+
+    return column_order, replace_by_order
+
+
+def _replace_value(df, eval_column, columns, lower_limit, replace_by):
+    df.loc[df[df[eval_column] < lower_limit].index, columns] = replace_by
     return df
 
 
--- tests/dataverk/utils/test_anonymize.py
+++ tests/dataverk/utils/test_anonymize.py
@@ -22,15 +22,19 @@ class MethodsReturnValues(TestCase):
         expected_df_out = pd.DataFrame(data={'col1': ["*", "*", "*", 4, 5, 6], 'col2': [3, 4, 5, 6, 7, 8]})
 
         df_out_additional_columns = anonymization.anonymize_replace(df_in_anonymizable_cols, eval_column='col1',
-                                                                    additional_columns=[])
+                                                                    additional_columns=None)
         self.assertTrue(df_out_additional_columns.equals(expected_df_out))
 
         df_out_lower_limit = anonymization.anonymize_replace(df_in_anonymizable_cols, eval_column='col1',
                                                              lower_limit=4)
         self.assertTrue(df_out_lower_limit.equals(expected_df_out))
 
-        df_out_both_defaults = anonymization.anonymize_replace(df_in_anonymizable_cols, eval_column='col1')
-        self.assertTrue(df_out_both_defaults.equals(expected_df_out))
+        df_out_replace_by = anonymization.anonymize_replace(df_in_anonymizable_cols, eval_column='col1',
+                                                            replace_by="*")
+        self.assertTrue(df_out_replace_by.equals(expected_df_out))
+
+        df_out_all_defaults = anonymization.anonymize_replace(df_in_anonymizable_cols, eval_column='col1')
+        self.assertTrue(df_out_all_defaults.equals(expected_df_out))
 
     def test_no_change_in_df_in(self):
         df_in_ = pd.DataFrame(data={'col1': [1, 2, 3, 4, 5, 6], 'col2': [33, 44, 55, 67, 765, 1111]})
@@ -44,6 +48,38 @@ class MethodsReturnValues(TestCase):
 
         df_both = anonymization.anonymize_replace(df_values, eval_column='ints', additional_columns='floats')
         self.assertTrue(df_both.equals(df_out_both))
+
+    def test_replace_by_single_value(self):
+        for r_val in [None, 1.5, 5]:
+            expected_df_out = pd.DataFrame(data={'col1': [r_val, r_val, r_val, 4, 5, 6],
+                                                 'col2': [r_val, r_val, r_val, 67, 765, 1111]})
+            df_out = anonymization.anonymize_replace(df_in, eval_column='col1', additional_columns=['col2'],
+                                                     lower_limit=4, replace_by=r_val)
+
+            self.assertTrue(df_out.equals(expected_df_out))
+
+    def test_replace_by_dict(self):
+        expected_df_out = pd.DataFrame(data={'col1': [None, None, None, 4, 5, 6],
+                                             'col2': [0, 0, 0, 67, 765, 1111]})
+        df_out = anonymization.anonymize_replace(df_in, eval_column='col1', additional_columns=['col2'],
+                                                     lower_limit=4, replace_by={'col2': 0, 'col1': None})
+
+        self.assertTrue(df_out.equals(expected_df_out))
+
+    def test_replace_by_dict_wrong_order(self):
+        expected_df_out = pd.DataFrame(data={'col1': [None, None, None, 4, 5, 6],
+                                             'col2': [0, 0, 0, 67, 765, 1111]})
+        df_out = anonymization.anonymize_replace(df_in, eval_column='col1', additional_columns=['col2'],
+                                                     lower_limit=4, replace_by={'col1': None, 'col2': 0})
+
+        self.assertTrue(df_out.equals(expected_df_out))
+
+    def test_replace_by_list(self):
+        expected_df_out = pd.DataFrame(data={'col1': [0, 0, 0, 4, 5, 6], 'col2': [None, None, None, 67, 765, 1111]})
+        df_out = anonymization.anonymize_replace(df_in, eval_column='col1', additional_columns=['col2'],
+                                                 lower_limit=4, replace_by=[None, 0])
+
+        self.assertTrue(df_out.equals(expected_df_out))
 
     def test_name_replace_env_not_set(self):
         df = pd.DataFrame(data={'name_column1': ["John Doe"], 'name_column2': ["Jane Doe"]})
@@ -60,7 +96,7 @@ class MethodsEvaluateInputTypes(TestCase):
             with self.assertRaises(TypeError):
                 anonymization.anonymize_replace(df_values, eval_column='ints', additional_columns=data_type)
 
-    def test_eval_column_type(self):
+    def test_eval_column_value_type(self):
         with self.assertRaises(TypeError):
             anonymization.anonymize_replace(df_values, eval_column='values')
 
@@ -89,3 +125,36 @@ class MethodsEvaluateInputTypes(TestCase):
 
         with self.assertRaises(ValueError):
             anonymization.anonymize_replace(df_values, eval_column='objects', additional_columns='object')
+
+    def test_column_name_types(self):
+        with self.assertRaises(TypeError):
+            anonymization.anonymize_replace(df_values, eval_column='ints', additional_columns=3)
+        with self.assertRaises(TypeError):
+            anonymization.anonymize_replace(df_values, eval_column=4, additional_columns='ints')
+
+    def not_to_replace_by(self):
+        not_to_replace_by = [{'1', '3'}, (1, 3), [1, [2, 3]]]
+        for r in not_to_replace_by:
+            with self.assertRaises(TypeError):
+                anonymization.anonymize_replace(df_values, eval_column='ints', additional_columns='floats', replace_by=r)
+
+    def invalid_number_in_replace_by_list_or_dict(self):
+        with self.assertRaises(Exception):
+            anonymization.anonymize_replace(df_values, eval_column='ints', additional_columns='floats', replace_by=[None, None, "*"])
+
+        with self.assertRaises(Exception):
+            anonymization.anonymize_replace(df_values, eval_column='ints', additional_columns='floats', replace_by=["*"])
+
+        with self.assertRaises(Exception):
+            anonymization.anonymize_replace(df_values, eval_column='ints', additional_columns='floats', replace_by={'ints': None, 'values': None, 'floats': "*"})
+
+        with self.assertRaises(Exception):
+            anonymization.anonymize_replace(df_values, eval_column='ints', additional_columns='floats', replace_by={'ints': "*"})
+
+    def invalid_key_names_in_replace_by_dict(self):
+        with self.assertRaises(Exception):
+            anonymization.anonymize_replace(df_values, eval_column='ints', additional_columns='floats', replace_by={'values': "*", 'flots': 0})
+
+    def invalid_key_name_types_in_replace_by_dict(self):
+        with self.assertRaises(Exception):
+            anonymization.anonymize_replace(df_values, eval_column='ints', additional_columns='floats', replace_by={'0': "*", 'flots': 0})
