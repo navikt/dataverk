2c96c52b403701eb48f36b0221dd138d22a7b90b
Vattekar <42180319+erikvatt@users.noreply.github.com>
2020-06-05 10:30:34 +0200

    anonymize is now moved to client api

--- src/dataverk/__init__.py
+++ src/dataverk/__init__.py
@@ -1,11 +1,9 @@
 from dataverk import utils
 from dataverk.dataverk import Dataverk
 from dataverk.dataverk import Dataverk as Client
-from dataverk.utils import anonymization
 from dataverk.datapackage import Datapackage
 
-__all__ = ['anonymization',
-           'connectors',
+__all__ = ['connectors',
            'utils',
            'Datapackage',
            'Client',
