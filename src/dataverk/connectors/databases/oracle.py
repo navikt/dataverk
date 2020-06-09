b3c05f552ac94dfd7cb6b35be0bf02e8cf778b21
Vattekar <42180319+erikvatt@users.noreply.github.com>
2020-06-09 16:04:58 +0200

    dask use sqlalchemy

--- src/dataverk/connectors/databases/oracle.py
+++ src/dataverk/connectors/databases/oracle.py
@@ -13,29 +13,16 @@ class OracleConnector(DBBaseConnector):
         super().__init__(settings_store, source)
 
     def get_dask_df(self, query, where_values) -> dd.DataFrame:
-        parsed_conn_string = parse_url(self.settings["db_connection_strings"][self.source])
-        return self._read_sql_query_dask(parsed_conn_string, query, where_values)
+        return self._read_sql_query_dask(query, where_values)
 
-    def _read_sql_query_dask(self, parsed_conn_string, sql, where_values):
+    def _read_sql_query_dask(self, sql, where_values):
         dload = dask.delayed(self._load_df_part)
-        parts = [dload(sql, parsed_conn_string, where) for where in where_values]
+        parts = [dload(sql, self._engine, where) for where in where_values]
         return dd.from_delayed(parts)
 
-    def _load_df_part(self, sql, parsed_conn_string, where):
-        dsn = cx_Oracle.makedsn(host=parsed_conn_string['host'], port=parsed_conn_string['port'],
-                                service_name=parsed_conn_string['service_name'])
-        db_conn = self._get_db_conn(parsed_conn_string, dsn)
+    def _load_df_part(self, sql, where):
         partial_sql = f"{sql} {where}"
-        return pd.read_sql(partial_sql, db_conn)
-
-    @staticmethod
-    def _get_db_conn(parsed_conn_string, dsn):
-        return cx_Oracle.connect(
-                user=parsed_conn_string['user'],
-                password=parsed_conn_string['password'],
-                dsn=dsn,
-                encoding='utf-8'
-            )
+        return pd.read_sql(partial_sql, self._engine)
 
     def _connection_string(self):
         connection_string = self.settings["db_connection_strings"][self.source]
