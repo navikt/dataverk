ea8c3a78c4a31ae3371572663c03479254053b51
Vattekar <42180319+erikvatt@users.noreply.github.com>
2020-06-30 16:08:12 +0200

    set name as title when name not provided

--- src/dataverk/views/remote_spec.py
+++ src/dataverk/views/remote_spec.py
@@ -8,7 +8,7 @@ class RemoteSpecView(BaseView):
     def __init__(self, name: str, resources: Sequence, title: str = "", description: str = "", attribution: str = "",
                  spec_type: str = "simple", spec: dict = None, type: str = "", group: str = "",
                  series: Sequence = list(), row_limit: int = 500, metadata: dict = None):
-        super().__init__(name=name, resources=resources, description=description, attribution=attribution,
+        super().__init__(name=name, resources=resources, title=title, description=description, attribution=attribution,
                          spec_type=spec_type, spec=spec, type=type, group=group,
                          series=series, row_limit=row_limit, metadata=metadata)
 
