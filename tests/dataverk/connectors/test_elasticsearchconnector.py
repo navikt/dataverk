3f71c9fbc0264d3d8bf83ea78cfc62fd976b0b59
Jorge Luis Lopez Marin <luis@Jorges-MacBook-Pro.local>
2020-09-17 11:05:23 +0200

    Adding test for elasticsearchconnector

--- tests/dataverk/connectors/test_elasticsearchconnector.py
+++ tests/dataverk/connectors/test_elasticsearchconnector.py
@@ -1,26 +1,56 @@
 from unittest import TestCase
 
 import requests
+import os
+
 
 from dataverk.connectors.elasticsearch import ElasticsearchConnector
+from dataverk.exceptions.dataverk_exceptions import IncompleteSettingsObject
 
+INDEX = "https://my.es.index.com"
+
+HOST = "elastic_host"
 
 SETTINGS = {
     "index_connections": {
-        "elastic_host": "https://my.es.index.com",
+        "elastic_host": INDEX,
         "index": "myindex",
         "token": "token"
     }
 }
 
+ENVIRON_KEY = "DATAVERK_ES_HOST"
+
 
 class Base(TestCase):
-    def setUp(self):
-        self.es = ElasticsearchConnector(SETTINGS)
+
+    def test__get_address_settings(self):
+        es = ElasticsearchConnector(SETTINGS, HOST)
+        address = es._get_es_address(SETTINGS, HOST)
+        actual_address = INDEX
+        self.assertEqual(actual_address, address)
+
+    def test__get_address_no_settings(self):
+        os.environ[ENVIRON_KEY] = INDEX
+        es = ElasticsearchConnector({}, HOST)
+        address = es._get_es_address({}, HOST)
+        actual_address = INDEX
+        self.assertEqual(actual_address, address)
+        os.environ.pop(ENVIRON_KEY)
+
+    def test__get_address_no_setting_env(self):
+        es = ElasticsearchConnector({}, HOST)
+        with self.assertRaises(IncompleteSettingsObject):
+            address = es._get_es_address({}, HOST)
 
 
 class MethodsReturnValues(Base):
 
     def test_write_invalid(self):
+        es = ElasticsearchConnector(SETTINGS, HOST)
         with self.assertRaises(requests.exceptions.RequestException):
-            self.es.write("id", {})
+            es.write("id", {})
+
+
+
+
