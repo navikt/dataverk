000f2b7f48f5933f8b8b9acba2ee41b154a590d2
Jorge Luis Lopez Marin <luis@Jorges-MacBook-Pro.local>
2020-09-17 12:29:38 +0200

    Reafactoring test

--- tests/dataverk/connectors/test_elasticsearchconnector.py
+++ tests/dataverk/connectors/test_elasticsearchconnector.py
@@ -26,22 +26,25 @@ class Base(TestCase):
 
     def test__get_address_settings(self):
         es = ElasticsearchConnector(SETTINGS, HOST)
-        address = es._get_es_address(SETTINGS, HOST)
+
+        address = es._es_address
         actual_address = INDEX
+
         self.assertEqual(actual_address, address)
 
     def test__get_address_no_settings(self):
         os.environ[ENVIRON_KEY] = INDEX
         es = ElasticsearchConnector({}, HOST)
-        address = es._get_es_address({}, HOST)
+
+        address = es._es_address
         actual_address = INDEX
+
         self.assertEqual(actual_address, address)
         del os.environ[ENVIRON_KEY]
 
     def test__get_address_no_settings_no_env(self):
         with self.assertRaises(dataverk_exceptions.IncompleteSettingsObject):
             es = ElasticsearchConnector({})
-
 
 
 class MethodsReturnValues(Base):
